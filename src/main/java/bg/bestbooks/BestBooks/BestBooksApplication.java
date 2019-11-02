9303f593bd74e03be21b4550b2d3ce1d1c76864a
kroxitrock <kroxitrock@gmail.com>
2019-11-02 16:56:48 +0200

    Added Swagger UI
    Swagger UI Added as a dependency and configured.

--- src/main/java/bg/bestbooks/BestBooks/BestBooksApplication.java
+++ src/main/java/bg/bestbooks/BestBooks/BestBooksApplication.java
@@ -2,8 +2,9 @@ package bg.bestbooks.BestBooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BestBooksApplication {
 
   public static void main(String[] args) {
