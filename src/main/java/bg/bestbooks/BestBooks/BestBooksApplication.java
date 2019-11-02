5d5a1ac3012c847cbeb92f394ad17267f3eaaac6
kroxitrock <kroxitrock@gmail.com>
2019-11-02 14:38:56 +0200

    Refactored code
    Refactored code to follow Google Style conventions.

--- src/main/java/bg/bestbooks/BestBooks/BestBooksApplication.java
+++ src/main/java/bg/bestbooks/BestBooks/BestBooksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BestBooksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BestBooksApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BestBooksApplication.class, args);
+  }
 
 }
