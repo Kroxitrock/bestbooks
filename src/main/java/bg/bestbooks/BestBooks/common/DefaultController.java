8bc73e252398bc63afabb8c5810b8ff6568e39f0
kroxitrock <kroxitrock@gmail.com>
2020-01-13 00:43:02 +0200

    [add] Index page

--- src/main/java/bg/bestbooks/BestBooks/common/DefaultController.java
+++ src/main/java/bg/bestbooks/BestBooks/common/DefaultController.java
@@ -1,14 +1,14 @@
 package bg.bestbooks.BestBooks.common;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DefaultController {
 
   @GetMapping
-  public ResponseEntity<String> healthCheck() {
-    return ResponseEntity.ok("System is up and running.");
+  public String healthCheck() {
+    return "index";
   }
 }
