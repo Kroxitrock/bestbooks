e439978750a0469f6e30fada3985e59b86152144
kroxitrock <kroxitrock@gmail.com>
2020-01-14 02:12:55 +0200

    [add] WIP book view

--- src/main/java/bg/bestbooks/BestBooks/common/DefaultController.java
+++ src/main/java/bg/bestbooks/BestBooks/common/DefaultController.java
@@ -2,13 +2,21 @@ package bg.bestbooks.BestBooks.common;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DefaultController {
 
   @GetMapping
-  public String healthCheck() {
+  public String index() {
     return "index";
+  }
+
+  @GetMapping(path = "/books/{id}")
+  public String getBooks(Model model, @PathVariable Integer id) {
+    model.addAttribute("id", id);
+    return "book";
   }
 }
--- src/main/java/bg/bestbooks/BestBooks/components/author/controller/AuthorsController.java
+++ src/main/java/bg/bestbooks/BestBooks/components/author/controller/AuthorsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/authors")
+@RequestMapping("api/authors")
 public class AuthorsController {
 
   private final AuthorsService authorsService;
